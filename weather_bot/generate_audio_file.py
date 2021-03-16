a0ae113814571573f6466dcdb053d0307ece5a3d
tinalimbu <tinalawati@gmail.com>
2021-03-16 23:56:27 +0000

    Modified generator

--- weather_bot/generate_audio_file.py
+++ weather_bot/generate_audio_file.py
@@ -4,15 +4,14 @@ from gtts import gTTS
 
 
 def generate_audio_file_from_text(
-  text, output_folder_path, file_name, file_type="mp3", language="en", slow=False
+  text, file_name, file_type="mp3", language="en", slow=False
 ):
   audio = gTTS(text=text, lang=language, slow=slow)
-  file_path = f"{output_folder_path}/{file_name}.{file_type}"
-  audio.save(file_path)
+  audio.save(f"{file_name}.{file_type}")
 
 
 if __name__ == "__main__":
   texts_and_file_names = [("Please could you repeat that?", "pardon")]
 
   for text, file_name in texts_and_file_names:
-    generate_audio_file_from_text(text, "media/common_audio_responses", file_name)
+    generate_audio_file_from_text(text, file_name)
