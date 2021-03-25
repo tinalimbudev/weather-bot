c5187fb8f02410936de7e315e066056f815b3c16
tinalimbu <tinalawati@gmail.com>
2021-03-25 12:26:30 +0000

    Used environment variable

--- weather_bot/api.py
+++ weather_bot/api.py
@@ -1,12 +1,12 @@
 import requests
 
-from settings import WEATHER_API_KEY
+from decouple import config
 
 
 def get_weather_for_today():
   # TODO: Make this more flexible.
+  api_key = config("WEATHER_API_KEY")
   response = requests.get(
-    f"http://api.openweathermap.org/data/2.5/weather?appid={WEATHER_API_KEY}&"
-    f"q=London"
+    f"http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q=London"
   )
   return response.json()
