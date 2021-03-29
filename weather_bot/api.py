d69bd66ab7301bae95fca0fa5a7a85320bc6787f
tinalimbu <tinalawati@gmail.com>
2021-03-29 18:36:06 +0100

    Updated exclude

--- weather_bot/api.py
+++ weather_bot/api.py
@@ -18,6 +18,6 @@ def get_weather_data(lang="en", units="metric", city=LONDON):
   lat, lon = CITY_TO_LAT_AND_LON_MAP.get(city, (LONDON_LAT, LONDON_LON))
   api_url = (
     f"https://api.openweathermap.org/data/2.5/onecall?appid={WEATHER_API_KEY}"
-    f"&lang={lang}&units={units}&lat={lat}&lon={lon}"
+    f"&lang={lang}&units={units}&lat={lat}&lon={lon}&exclude=minutely,alerts"
   )
   return requests.get(api_url).json()
