876b7760fd04b97beb86a2c7ffaa4470ba7d49d7
tinalimbu <tinalawati@gmail.com>
2021-03-29 17:30:31 +0100

    Improved API

--- weather_bot/api.py
+++ weather_bot/api.py
@@ -6,9 +6,9 @@ from decouple import config
 WEATHER_API_KEY = config("WEATHER_API_KEY")
 
 
-def get_weather_for_today():
-  response = requests.get(
-    f"http://api.openweathermap.org/data/2.5/weather?appid={WEATHER_API_KEY}&"
-    f"q=London"
+def get_current_weather(city_name="London", units="metric"):
+  api_url = (
+    f"http://api.openweathermap.org/data/2.5/weather?appid={WEATHER_API_KEY}"
+    f"&q={city_name}&units={units}"
   )
-  return response.json()
+  return requests.get(api_url).json()
