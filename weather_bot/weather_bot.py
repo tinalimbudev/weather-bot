1b37f45b9b9cd5c7ecfb1bb4a59128e330cb7620
tinalimbu <tinalawati@gmail.com>
2021-03-30 18:38:31 +0100

    Modified weather bot

--- weather_bot/weather_bot.py
+++ weather_bot/weather_bot.py
@@ -1,10 +1,15 @@
 import speech_recognition as sr
 
-from helpers import hello, ask_name, greet, ask_today_or_not, report_current_weather
+from api import get_full_weather_data, extract_weather_data
+from helpers import hello, ask_name, greet, ask_today_or_not, report_current_weather, ask_current_or_not, ask_how_many_hours, ask_how_many_days, get_weather_data_for_later_time, report_weather_for_later_time, get_weather_data_for_later_day, report_weather_for_later_day
 
 
 def run_weather_bot():
   recognizer = sr.Recognizer()
+  call_dt, data = get_full_weather_data()
+  current_data = data["current"]
+  hourly_data = data["hourly"]
+  daily_data = data["daily"]
 
   with sr.Microphone() as audio_source:
     recognizer.adjust_for_ambient_noise(audio_source, duration=0.5)
@@ -15,18 +20,28 @@ def run_weather_bot():
     today_or_not = ask_today_or_not(audio_source, recognizer)
 
     if "today" in today_or_not:
-      # if now
-      report_current_weather()
-      # if another time then get another time
+      current_or_not = ask_current_or_not(audio_source, recognizer)
+
+      if "current" in current_or_not:
+        temp, feels_like = extract_weather_data(current_data)
+        report_current_weather(temp, feels_like)
+      elif "different time" in current_or_not:
+        num_of_hours = ask_how_many_hours(audio_source, recognizer)
+        data = get_weather_data_for_later_time(
+          call_dt, hourly_data, num_of_hours
+        )
+        temp, feels_like = extract_weather_data(data)
+        report_weather_for_later_time(temp, feels_like)
+        # TODO: Handle invalid number of hours
 
     elif "different day" in today_or_not:
-      # Get date and time and get report
-      pass
-
-    else:
-      # Try to get today or a different date again.
-      # try to get time again
-      pass
+      num_of_days = ask_how_many_days(audio_source, recognizer)
+      data = get_weather_data_for_later_day(
+        call_dt, daily_data, num_of_days
+      )
+      temp, feels_like = extract_weather_data(data)
+      report_weather_for_later_day(temp, feels_like)
+      # TODO: Handle invalid number of days
 
     # Check if they want a different query
     # If not then exit
