763020d887782a811cd70cc11d7dcce13d506676
tinalimbu <tinalawati@gmail.com>
2021-03-30 13:24:09 +0100

    Tidied up API

--- weather_bot/api.py
+++ weather_bot/api.py
@@ -1,5 +1,6 @@
 import requests
 
+from datetime import datetime, timedelta
 from decouple import config
 
 
@@ -15,42 +16,20 @@ CITY_TO_LAT_AND_LON = {
 
 
 def get_weather_data(lang="en", units="metric", city=LONDON):
-  lat, lon = CITY_TO_LAT_AND_LON_MAP.get(city, (LONDON_LAT, LONDON_LON))
+  call_dt = datetime.now().replace(microsecond=0)
+
+  lat, lon = CITY_TO_LAT_AND_LON.get(city, (LONDON_LAT, LONDON_LON))
   api_url = (
     f"https://api.openweathermap.org/data/2.5/onecall?appid={WEATHER_API_KEY}"
     f"&lang={lang}&units={units}&lat={lat}&lon={lon}&exclude=minutely,alerts"
   )
-  return requests.get(api_url).json()
+  data = requests.get(api_url).json()
+
+  return call_dt, data
 
 
-def get_current_weather(data):
+def extract_current_weather(data):
   current_data = data["current"]
   temp = current_data["temp"]
   feels_like = current_data["feels_like"]
   return temp, feels_like
-
-
-def get_weather_after_some_hours(data, num_of_hours):
-  # TODO
-  return data
-
-
-def get_weather_after_some_days(data, num_of_days):
-  # TODO
-  return data
-
-
-# def report_current_weather():
-#   data = get_weather_data()
-
-#   weather = data["weather"]["main"]
-#   temp = data["main"]["temp"]
-#   feels_like = data["main"]["feels_like"]
-#   temp_min = data["main"]["temp_min"]
-#   temp_max = data["main"]["temp_max"]
-
-#   respond_dynamically(
-#     f"Today is looking {weather}. The current temperature is {temp} degrees "
-#     f"celcius and feels like {feels_like}. The coldest today will be is "
-#     f"{temp_min} degrees celcius, and the warmest today will be is {temp_max}."
-#   )
