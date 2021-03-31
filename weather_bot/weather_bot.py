cbaab74882fc4a26ecfde20f6e29a2342f914d98
tinalimbu <tinalawati@gmail.com>
2021-03-31 10:15:22 +0100

    Refactored weather bot

--- weather_bot/weather_bot.py
+++ weather_bot/weather_bot.py
@@ -4,22 +4,18 @@ from api import get_full_weather_data, extract_weather_data, get_weather_data_fo
 from helpers import hello, ask_name, greet, ask_today_or_not, report_current_weather, ask_current_or_not, ask_how_many_hours, ask_how_many_days, report_weather_for_later_time, report_weather_for_later_day, ask_if_query_again, goodbye
 
 
-def query_weather_bot(audio_source, recognizer):
-  call_dt, data = get_full_weather_data()
-  current_data = data["current"]
-  hourly_data = data["hourly"]
-  daily_data = data["daily"]
+def query_weather_bot(call_dt, data, audio_source, recognizer):
   today_or_not = ask_today_or_not(audio_source, recognizer)
-  # TODO: Check responses are expected else get response again.
-  # TODO: Only give extra info like forcast up to 12 hours the first time.
 
   if "today" in today_or_not:
     current_or_not = ask_current_or_not(audio_source, recognizer)
 
     if "current" in current_or_not:
+      current_data = data["current"]
       temp, feels_like = extract_weather_data(current_data)
       report_current_weather(temp, feels_like)
     elif "different time" in current_or_not:
+      hourly_data = data["hourly"]
       num_of_hours = ask_how_many_hours(audio_source, recognizer)
       data = get_weather_data_for_later_time(
         call_dt, hourly_data, num_of_hours
@@ -29,6 +25,7 @@ def query_weather_bot(audio_source, recognizer):
       # TODO: Handle invalid number of hours
 
   elif "different day" in today_or_not:
+    daily_data = data["daily"]
     num_of_days = ask_how_many_days(audio_source, recognizer)
     data = get_weather_data_for_later_day(
       call_dt, daily_data, num_of_days
@@ -40,17 +37,18 @@ def query_weather_bot(audio_source, recognizer):
 
 def run_weather_bot():
   recognizer = sr.Recognizer()
-  query = True
 
   with sr.Microphone() as audio_source:
     recognizer.adjust_for_ambient_noise(audio_source, duration=0.5)
+    call_dt, data = get_full_weather_data()
+    query = True
 
     hello()
     name = ask_name(audio_source, recognizer)
     greet(name)
 
     while query:
-      query_weather_bot(audio_source, recognizer)
+      query_weather_bot(call_dt, data, audio_source, recognizer)
       query_again = ask_if_query_again(audio_source, recognizer)
 
       if "no" in query_again:
