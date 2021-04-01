86c86b483473e3959d4e13130996126c363c8f2b
tinalimbu <tinalawati@gmail.com>
2021-04-01 14:14:38 +0100

    Tidied up weather bot

--- weather_bot/weather_bot_old.py
+++ weather_bot/weather_bot_old.py
@@ -1,29 +1,36 @@
 import speech_recognition as sr
 
-from api import get_full_weather_data, extract_weather_data, get_weather_data_for_later_time, get_weather_data_for_later_day
-from helpers import hello, ask_name, greet, ask_today_or_not, report_current_weather, ask_current_or_not, ask_how_many_hours, ask_how_many_days, report_weather_for_later_time, report_weather_for_later_day, ask_if_query_again, goodbye, QueryOptions
+from api import get_weather_data
+from query import QueryOptions, query_weather_bot
+from response import (
+  ask_if_query_again,
+  ask_name,
+  greet,
+  say_goodbye,
+  say_hello,
+)
 
 
 def run_weather_bot():
   recognizer = sr.Recognizer()
 
-  with sr.Microphone() as audio_source:
-    recognizer.adjust_for_ambient_noise(audio_source, duration=0.5)
-    call_dt, data = get_full_weather_data()
+  with sr.Microphone() as source:
+    recognizer.adjust_for_ambient_noise(source, duration=0.5)
+    call_dt, data = get_weather_data()
     query = True
 
-    hello()
-    name = ask_name(audio_source, recognizer)
+    say_hello()
+    name = ask_name(source, recognizer)
     greet(name)
 
     while query:
-      query_weather_bot(call_dt, data, audio_source, recognizer)
-      query_again = ask_if_query_again(audio_source, recognizer)
+      query_weather_bot(call_dt, data, source, recognizer)
+      query_again = ask_if_query_again(source, recognizer)
 
       if QueryOptions.do_not_query_again.value in query_again:
         query = False
 
-    goodbye()
+    say_goodbye()
 
 
 if __name__ == "__main__":
